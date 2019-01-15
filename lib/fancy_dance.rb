f067eb393e227af8df66c2dee36def18218f403c
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-01-15 06:55:29 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -14,3 +14,15 @@ module FancyDance
     end
 
     def take_a_bow
+      "Thank you, thank you. It was a pleasure to dance for you all."
+    end
+  end
+
+  module ClassMethods
+
+    def metadata
+      "This class produces objects that love to dance."
+    end
+  end
+end
+ 
